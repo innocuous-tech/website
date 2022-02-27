9e45f87b7697cba00b8e5ab3cf04567332ac10a1
Kyle Holmberg <kylemh.email@gmail.com>
2022-02-26 19:18:32 -0500

    lang="en"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ import { getCssText } from '~/stitches.config';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,900;1,400;1,600;1,900&display=swap"
